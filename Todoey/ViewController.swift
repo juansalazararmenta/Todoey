1df5ae242dee11fffd3e0a1c2029965183fa1626
Juan Salazar Armenta <juansalazararmenta@Juans-iMac.local>
2018-09-09 13:07:12 -0500

    First Commit. Empty Project

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Juan Salazar Armenta on 9/9/18.
+//  Copyright © 2018 Juan Salazar Armenta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
